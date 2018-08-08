66e432e58f0c22cb0efa6fc8c62e246febf8f6a9
Makan Azarshahy <oro1919@gmail.com>
2018-08-07 20:59:57 -0700

    Added dummy data

---
+++ app/dummyData/index.js
@@ -0,0 +1,39 @@
+const todos = [
+  {
+    id: '1jd912k',
+    text: 'Add a new component',
+    date: 'Jan 5, 2018',
+    difficulty: 'Medium',
+    notes: 'When adding a new component, be sure to examine what kind of component it will be',
+  },
+  {
+    id: '82hkas3',
+    text: 'Review containers vs presentational components',
+    date: 'Mar 12, 2018',
+    difficulty: 'Hard',
+    notes: 'Containers can be difficult to understand, but with time you\'ll get it',
+  },
+  {
+    id: 'h7js832',
+    text: 'Refactor state for immutability',
+    date: 'Apr 22, 2018',
+    difficulty: 'Hard',
+    notes: 'When you have a mutable state, be prepared for an onslaught of bugs. #BugHunt',
+  },
+  {
+    id: '92uhu3i',
+    text: 'Read an interesting blog on functional programming',
+    date: 'May 13, 2018',
+    difficulty: 'Easy',
+    notes: 'What is it? Honestly, I\'m still trying to figure it out',
+  },
+  {
+    id: 'qwerty',
+    text: 'Grab a bagel',
+    date: 'Jun 5, 2018',
+    difficulty: 'Extra Hard',
+    notes: 'Don\'t be fooled by this task. It can be pretty hard to find a good bagel...',
+  },
+];
+
+export default todos;
