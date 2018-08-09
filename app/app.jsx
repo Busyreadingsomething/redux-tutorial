0e1a3a12660ce6d19dc819fb735256e8050a2240
Makan Azarshahy <oro1919@gmail.com>
2018-08-09 00:19:12 -0700

    Added Provider import from react-redux

--- app/app.jsx
+++ app/app.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'; // eslint-disable-line 
 import Todos from './components/Todos';
 
 ReactDOM.render(<Todos />, document.getElementById('app'));
