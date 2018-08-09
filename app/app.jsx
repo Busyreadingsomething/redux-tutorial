40af0283bf0b94655896c86699d516141519de38
Makan Azarshahy <oro1919@gmail.com>
2018-08-09 14:46:44 -0700

    Updated todos and removed the provider

--- app/app.jsx
+++ app/app.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'; // eslint-disable-line 
 import Todos from './components/Todos';
 
 ReactDOM.render(<Todos />, document.getElementById('app'));
