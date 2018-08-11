aac8a1f6ccc96bb609682da335e479a2dad4938e
Makan Azarshahy <oro1919@gmail.com>
2018-08-11 15:00:40 -0700

    Added notes to guide the user on what to do for the container

--- app/containers/Item/container.jsx
+++ app/containers/Item/container.jsx
@@ -2,6 +2,23 @@ import React from 'react';
 import { func, shape, string } from 'prop-types';
 import Details from '../../components/Details';
 
+/**
+  The component below is a copy and paste from the components folder. You're tasked with the job to convert this component into
+  a stateless functional component with redux. YOU WILL NOT NEED TO ADJUST THE FILES IN THE COMPONENTS FOLDER TO DO ANYTHING.
+
+  You will be designing the container here, so your mapStateToProps and mapDispatchToProps will be used here.
+
+  HINTS:
+  - Read the documentation
+  - Follow the tests (they're designed to guide you along the way)
+  - Take your time and reflect on your approach before you change anything.
+
+  NOTES:
+  - There are a lot of different ways people organize their code, so don't assume this is the only way to write redux code
+  - Remember to evaluate the differences between the component and the container once you have completed the tests and made
+    everything render properly
+*/
+
 class Item extends React.Component {
   constructor(props) {
     super(props);
