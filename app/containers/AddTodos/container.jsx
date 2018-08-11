9a4333b8725ea9cd53431f9d86d44f6d27f8e7a0
Makan Azarshahy <oro1919@gmail.com>
2018-08-11 14:50:52 -0700

    Added base for the container. Users will have to modify them for their reference

---
+++ app/containers/AddTodos/container.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import Input from '../../components/Input';
+
+class AddTodo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      types: ['text', 'difficulty', 'notes'],
+      text: '',
+      difficulty: '',
+      date: '',
+      notes: '',
+    };
+  }
+
+  handleChange(e) {
+    const { value } = e.target;
+    const type = e.target.getAttribute('data-type');
+    const update = {
+      [type]: value,
+    };
+    this.setState(state => Object.assign({}, state, update), () => console.log(this.state));
+  }
+
+  render() {
+    const { types } = this.state;
+
+    return (
+      <div className="add-todo-container">
+        {
+          types.map(type => (
+            <Input
+              key={type}
+              type={type}
+              change={e => this.handleChange(e)}
+              val={this.state[type]}
+            />))
+        }
+      </div>
+    );
+  }
+}
+
+export default AddTodo;
