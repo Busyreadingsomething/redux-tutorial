c2ff6ca5c3de7c52c4fa8eab9936433dd147a87b
Makan Azarshahy <oro1919@gmail.com>
2018-08-12 16:59:56 -0700

    Changed functional component to PureComponent

--- app/components/List.jsx
+++ app/components/List.jsx
@@ -7,31 +7,35 @@ import {
 } from 'prop-types';
 import Item from './Item';
 
-const filtering = (todos, visibilityFilter) => {
-  let filtered = todos;
-  if (visibilityFilter === 'SHOW_COMPLETED') {
-    filtered = todos.filter(todo => todo.completed === true);
-  } else if (visibilityFilter === 'SHOW_ACTIVE') {
-    filtered = todos.filter(todo => todo.completed === false);
+class List extends React.PureComponent {
+  filtering() {
+    const { todos, filter } = this.props;
+    let filtered = todos;
+    if (filter === 'SHOW_COMPLETED') {
+      filtered = todos.filter(todo => todo.completed === true);
+    } else if (filter === 'SHOW_ACTIVE') {
+      filtered = todos.filter(todo => todo.completed === false);
+    }
+    return filtered;
   }
-  return filtered;
-};
 
-const List = ({ todos, toggle, filter }) => {
-  const filtered = filtering(todos, filter);
-  return (
-    <div className="list">
-      <p className="list-title">
-        Current Todos
-      </p>
-      <div className="list-content">
-        {
-          filtered.map(todo => <Item key={todo.id} todo={todo} toggle={toggle} />)
-        }
+  render() {
+    const { todos, toggle, filter } = this.props;
+    const filtered = this.filtering(todos, filter);
+    return (
+      <div className="list">
+        <p className="list-title">
+          Current Todos
+        </p>
+        <div className="list-content">
+          {
+            filtered.map(todo => <Item key={todo.id} todo={todo} toggle={toggle} />)
+          }
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 List.propTypes = {
   todos: arrayOf(object).isRequired,
