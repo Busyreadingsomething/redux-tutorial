bf8bfd1f723dcda2e35c54c0a6e70c948c34affe
Makan Azarshahy <oro1919@gmail.com>
2018-08-12 13:41:14 -0700

    Added basic logic to filter out the appropriate todos

--- app/components/List.jsx
+++ app/components/List.jsx
@@ -1,23 +1,42 @@
 import React from 'react';
-import { arrayOf, func, object } from 'prop-types';
+import {
+  arrayOf,
+  func,
+  object,
+  string,
+} from 'prop-types';
 import Item from './Item';
 
-const List = ({ todos, toggle }) => (
-  <div className="list">
-    <p className="list-title">
-      Current Todos
-    </p>
-    <div className="list-content">
-      {
-        todos.map(todo => <Item key={todo.id} todo={todo} toggle={toggle} />)
-      }
+const filtering = (todos, visibilityFilter) => {
+  let filtered = todos;
+  if (visibilityFilter === 'SHOW_COMPLETED') {
+    filtered = todos.filter(todo => todo.completed === true);
+  } else if (visibilityFilter === 'SHOW_ACTIVE') {
+    filtered = todos.filter(todo => todo.completed === false);
+  }
+  return filtered;
+};
+
+const List = ({ todos, toggle, filter }) => {
+  const filtered = filtering(todos, filter);
+  return (
+    <div className="list">
+      <p className="list-title">
+        Current Todos
+      </p>
+      <div className="list-content">
+        {
+          filtered.map(todo => <Item key={todo.id} todo={todo} toggle={toggle} />)
+        }
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 List.propTypes = {
   todos: arrayOf(object).isRequired,
   toggle: func.isRequired,
+  filter: string.isRequired,
 };
 
 export default List;
