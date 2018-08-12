4ea481c118913976b3985324b7e5467f959c9fd8
Makan Azarshahy <oro1919@gmail.com>
2018-08-12 13:40:19 -0700

    Fixed code readability fo the actions file

--- app/containers/Todos/actions.js
+++ app/containers/Todos/actions.js
@@ -1,23 +1,22 @@
 import constants from './constants';
 
 /**
- * You are going to design your actions in this folder. Keeping your code organized is far more important in the long run
- * thank trying to make something that workds immediately. Coding is a team sport so always remember that other people
- * adopt your code at some point to work on it.
- * 
+ * You are going to design your actions in this folder. Keeping your code organized is far more
+ * important in the long run thank trying to make something that workds immediately. Coding is
+ * a team sport so always remember that other people adopt your code at some point to work on it.
+ *
  * Below is an example of an action. Only this file will have an example for you to use.
- * 
+ *
  * Add the necessary actions in this file and be sure to export them.
- * 
+ *
  * Be sure to remove the example when you are done.
- * 
+ *
  * HINTS:
  * - Actions always return an object with a type property
  * - Additional properties are not always requried
- * 
+ *
  * NOTES:
  * - Be naming is important, so be sure to check the tests for the expected names
- * 
  */
 
 const ACTION = data => ({
