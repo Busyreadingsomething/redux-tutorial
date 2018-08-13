317737568a41a390dd948163a0e2b5c986f8827d
Makan Azarshahy <oro1919@gmail.com>
2018-08-13 12:10:04 -0700

    Added getDate method to get the current Date, and the handleClick to handle adding the todo

--- app/components/AddTodo.jsx
+++ app/components/AddTodo.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import { func } from 'prop-types';
 import Input from './Input';
+
+export const getDate = () => {
+  const date = [];
+  const today = new Date();
+  date.push(today.getMonth() + 1);
+  date.push(today.getDate());
+  date.push(today.getFullYear());
+  return date.join('/');
+};
 
 class AddTodo extends React.Component {
   constructor(props) {
@@ -8,7 +18,6 @@ class AddTodo extends React.Component {
       types: ['text', 'difficulty', 'notes'],
       text: '',
       difficulty: '',
-      date: '',
       notes: '',
     };
   }
@@ -20,6 +29,24 @@ class AddTodo extends React.Component {
       [type]: value,
     };
     this.setState(state => Object.assign({}, state, update), () => console.log(this.state));
+  }
+
+  handleClick() {
+    const { text, difficulty, notes } = this.state;
+    const { add } = this.props;
+    const date = getDate();
+    const todo = {
+      text,
+      difficulty,
+      notes,
+      date,
+    };
+    const empty = {
+      text: '',
+      difficulty: '',
+      notes: '',
+    };
+    this.setState(state => Object.assign({}, state, empty), () => add(todo));
   }
 
   render() {
@@ -36,9 +63,14 @@ class AddTodo extends React.Component {
               val={this.state[type]}
             />))
         }
+        <button type="button" onClick={() => this.handleClick()}>ADD TODO</button>
       </div>
     );
   }
 }
+
+AddTodo.propTypes = {
+  add: func.isRequired,
+};
 
 export default AddTodo;
