e7631117c00e23fdb490a56477ce14b7ebdbe5d3
Makan Azarshahy <oro1919@gmail.com>
2018-08-13 01:42:04 -0700

    Added 100% test coverage for the Details component

--- app/components/tests/Details.test.js
+++ app/components/tests/Details.test.js
@@ -13,4 +13,16 @@ describe('<Details />', () => {
   it('should render div wrapper with class overlay', () => {
     expect(wrapper.find('div.overlay').length).toBe(1);
   });
+
+  it('should render p tag with class date', () => {
+    expect(wrapper.find('p.date').length).toBe(1);
+  });
+
+  it('should render p tag with class difficulty', () => {
+    expect(wrapper.find('p.difficulty').length).toBe(1);
+  });
+
+  it('should render h4 tag with class notes', () => {
+    expect(wrapper.find('h4.notes').length).toBe(1);
+  });
 });
