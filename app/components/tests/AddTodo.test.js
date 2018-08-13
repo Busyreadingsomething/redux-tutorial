e9419036f2fdf9c0fdc9f10826c7101885d0cfb0
Makan Azarshahy <oro1919@gmail.com>
2018-08-13 01:42:23 -0700

    Added 100% test coverage for AddTodo Component

--- app/components/tests/AddTodo.test.js
+++ app/components/tests/AddTodo.test.js
@@ -4,8 +4,29 @@ import AddTodo from '../AddTodo';
 
 /* eslint-disable */
 describe('<AddTodo />', () => {
+  const wrapper = shallow(<AddTodo />);
+  const mockEvent = {
+    target: {
+      value: 'TESTING',
+      getAttribute() {
+        return 'text';
+      },
+    },
+  };
+
   it('should render div wrapper with class add-todo-container', () => {
-    const wrapper = shallow(<AddTodo />);
     expect(wrapper.find('div.add-todo-container').length).toBe(1);
+  });
+
+  it('should have Input components rendered', () => {
+    expect(wrapper.find('Input').exists()).toBe(true);
+  });
+
+  it('should have Input with the a working change prop calling the hangleChange', () => {
+    expect(wrapper.state().text).toBe('');
+    const change = wrapper.find('Input').first().prop('change');
+    expect(change).toBeInstanceOf(Function);
+    change(mockEvent);
+    expect(wrapper.state().text).toBe('TESTING');
   });
 });
